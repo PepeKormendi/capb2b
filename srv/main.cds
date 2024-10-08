904208157816ccb086bd668f7a90959561a6ad31
PepeKormendi <98765784+PepeKormendi@users.noreply.github.com>
2024-10-08 16:30:01 +0000

    urgency virtual

--- srv/main.cds
+++ srv/main.cds
@@ -1,5 +1,8 @@
 using org from '../db/schema';
 
+extend org.peterkor.Books with {
+    virtual urgency: String;
+}
 service bookshop {
     entity Books   as projection on org.peterkor.Books;
     entity Authors as projection on org.peterkor.Authors;
--- srv/main.js
+++ srv/main.js
@@ -1,13 +1,11 @@
 const cds = require("@sap/cds");
 const logger = cds.log('capb2b')
 module.exports = cds.service.impl(function () {
-  console.log("i am in the function");
+  logger( new Date());
   this.on('READ', 'Books', (req, next) => {
   
     return next() 
   })
   this.before('READ', 'Books', () => console.log("before Read books"))
-  this.after('READ', 'Books', (data, req) => {
-    data.map(book => book.title += '!!!!')
-  })
+  this.after('each', 'Books', book => book.urgency = 'URG' )
 });
