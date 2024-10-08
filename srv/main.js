7649298cf6a3a60955785b993d42c7cd963c9996
PepeKormendi <98765784+PepeKormendi@users.noreply.github.com>
2024-10-08 13:22:54 +0000

    lesson 15 begining. new plugin installeD

--- srv/main.js
+++ srv/main.js
@@ -1,12 +1,13 @@
 const cds = require("@sap/cds");
+const logger = cds.log('capb2b')
 module.exports = cds.service.impl(function () {
   console.log("i am in the function");
-  this.on('READ', 'Books', async (req, next) => {
-    console.log(req.target.kind)
-    const result = await next();
-    console.log(req.data);
-    return result;
-  } )
-  this.before('READ', 'Books', () => console.log("before Read books") )
-  this.after('READ', 'Books', () => console.log("after Read books") )
+  this.on('READ', 'Books', (req, next) => {
+  
+    return next() 
+  })
+  this.before('READ', 'Books', () => console.log("before Read books"))
+  this.after('READ', 'Books', (data, req) => {
+    data.map(book => book.title += '!!!!')
+  })
 });
